d1bc4661df80e433847129dc29a5e2f102bb6f54
abbadon1334 <fdanti@gmail.com>
2019-07-12 20:48:34 +0300

    doc compiled with sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,14 +39,14 @@ release = u'1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
+    #'sphinx.ext.autodoc',
+    #'sphinx.ext.doctest',
+    #'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
+    #'sphinx.ext.coverage',
+    #'sphinx.ext.mathjax',
+    #'sphinx.ext.ifconfig',
+    #'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinxcontrib.phpdomain'
 ]
@@ -84,7 +84,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
